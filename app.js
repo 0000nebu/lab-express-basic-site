52459c285bd56099614685a6eca7049c3900a042
0000nebu <neboa.fresco@gmail.com>
2023-07-18 00:18:13 +0200

    solved lab

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const app = express();
+
+//configre rendering views engine 
+app.set('view engine', 'hbs');
+app.set ('views', `${__dirname}/views`)
+
+app.get('/', (req, res, next)=> {
+    res.render(`home`);
+})
+
+app.get('/about', (req, res, next) => {
+    res.render(`about`)
+})
+
+app.get('/works', (req, res,next)=>{
+    res.render('works')
+})
+
+app.get('/photo-gallery', (req, res, next )=> {
+    res.render('photo-gallery')
+})
+
+const port = 3000;
+app.listen(port,() => console.info(`application running at port ${port}`));
